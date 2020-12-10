a4ef017e48456cda12be3868a18f09e2c52063cf
newsning <newsning@sina.com>
2020-12-11 00:24:57 +0800

    feat: add routers with middleware

---
+++ god/trie.go
@@ -0,0 +1,27 @@
+package god
+
+type node struct {
+	pattern  string
+	part     string
+	children []*node
+	isWild   bool
+}
+
+func (n *node) matchChild(part string) *node {
+	for _, child := range n.children {
+		if child.part == part || child.isWild {
+			return child
+		}
+	}
+	return nil
+}
+
+func (n *node) matchChildren(part string) []*node {
+	nodes := make([]*node, 0)
+	for _, child := range n.children {
+		if child.part == part || child.isWild {
+			nodes = append(nodes, child)
+		}
+	}
+	return nodes
+}
